b32c611756f5f4fcc77effa8db240f3ac767579f
Núcleo de Robótica do Cesmac <roboticacesmac@gmail.com>
2025-01-31 15:37:55 -0300

    Tentativa de ajuste do nixpacks.toml para funcionamento do sqlite no Coolify

--- start.sh
+++ start.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
 
 # Ajusta as permissões do diretório database
-echo "Ajustando permissões de /app/app/database..."
+echo "Iniciando o script de inicialização..." > /app/start.log
 
 # Verifica se o grupo 'nobody' existe, caso contrário, cria o grupo
+echo "Criando o grupo nobody se não existir" >> /app/start.log
 if ! grep -q '^nobody:' /etc/group; then
   addgroup nobody
 fi
 
 # Ajusta as permissões para o diretório /app/app/database
+echo "Ajustando permissões de /app/app/database..." >> /app/start.log
 chown -R nobody:nobody /app/app/database
 chmod -R 750 /app/app/database
+
+# Verifica as permissões após ajuste
+echo "Verificando permissões de /app/app/database..." >> /app/start.log
+ls -ld /app/app/database >> /app/start.log
+ls -l /app/app/database >> /app/start.log
