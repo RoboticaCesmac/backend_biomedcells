facb64085857940ef08bb2ed9d68088fde493f9b
Núcleo de Robótica do Cesmac <roboticacesmac@gmail.com>
2025-01-31 11:09:23 -0300

    Tentativa de ajuste do nixpacks.toml e adicionado start.sh, para funcionamento do sqlite no Coolify

---
+++ start.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Ajusta as permissões do diretório database
+echo "Ajustando permissões de /app/app/database..."
+chown -R nobody:nobody /app/app/database
+chmod -R 750 /app/app/database
